776aa028abf1c10cb47dc759137032ddba23713f
incshaun <mail@nowhere.there>
2024-03-21 16:05:11 +1100

    Current steps for building some 3rd party libraries.

--- deps/updateDeps.sh
+++ deps/updateDeps.sh
@@ -46,3 +46,26 @@ if [ ! -f ../externals/FreeImage/libfreeimage-3.16.0.so ]; then
   cp libfreeimage-3.16.0.so ../../deps/FreeImage/Release/linux-x64/libFreeImage.so
   )
 fi
+
+# PVR Tex lib
+if [ ! -f PVRTT/linux-x64/libPVRTexLib.so ]; then
+  (
+  cd PVRTT/source/
+  mkdir ../linux-x64
+  clang++ -m64 --shared -I../include/ -O3 -Wall -Wno-unused-variable -stdlib=libc++ -std=c++14 -fPIC  *.cpp -o ../linux-x64/libPVRTexLib.so -ldl
+  # copy to somewhere relevant.
+  )
+fi
+
+# DirectXTex
+(
+  git clone --recursive https://github.com/microsoft/DirectXTex.git ../externals/DirectXTex
+#  https://github.com/microsoft/DirectXMath
+# https://github.com/microsoft/DirectX-Headers
+# https://raw.githubusercontent.com/tpn/winsdk-10/master/Include/10.0.10240.0/shared/sal.h
+# clang++ -m64 --shared -I. -I../DirectXMath-main/Inc/ -I../DirectX-Headers-main/include/ -I../DirectX-Headers-main/include/wsl/stubs -I../include/ -O3 -Wall -Wno-unused-variable -stdlib=libc++ -std=c++17 -fPIC  dxt_wrapper.cpp DirectXTexDDS.cpp DirectXTexConvert.cpp DirectXTexCompress.cpp DirectXTexMipmaps.cpp DirectXTexResize.cpp DirectXTexNormalMaps.cpp -o ../linux-x64/libDxtWrapper.so  -ldl -fdeclspec
+clang++ -m64 --shared -I. -I../DirectXMath-main/Inc/ -I../DirectX-Headers-main/include/ -I../DirectX-Headers-main/include/wsl/stubs -I../include/ -O3 -Wall -Wno-unused-variable -stdlib=libc++ -std=c++17 -fPIC  dxt_wrapper.cpp DirectXTexDDS.cpp DirectXTexConvert.cpp DirectXTexCompress.cpp DirectXTexMipmaps.cpp DirectXTexResize.cpp DirectXTexNormalMaps.cpp DirectXTexImage.cpp DirectXTexUtil.cpp BC6HBC7.cpp BC.cpp BC4BC5.cpp DirectXTexTGA.cpp DirectXTexPMAlpha.cpp -o ../linux-x64/libDxtWrapper.so  -ldl -fdeclspec -Wl,-z,defs
+cp ../linux-x64/libDxtWrapper.so ../../../sources/editor/Stride.GameStudio.Avalonia/bin/Debug/net8.0-windows7.0/DxtWrapper.so 
+cp ../linux-x64/libDxtWrapper.so ../../../sources/editor/Stride.GameStudio.Avalonia/bin/Debug/net8.0-windows7.0/runtimes/linux-x64/native/
+
+)
