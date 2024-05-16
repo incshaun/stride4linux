84a409a4e8d410dbd5b5e76e3febc34af2dbe8d2
incshaun <mail@nowhere.there>
2024-05-16 15:15:50 +1000

    Translated more templates, to complete the set

--- sources/editor/Stride.Assets.Presentation.Avalonia/Templates/StrideTemplates.cs
+++ sources/editor/Stride.Assets.Presentation.Avalonia/Templates/StrideTemplates.cs
@@ -10,26 +10,26 @@ namespace Stride.Assets.Presentation.Templates
         public static void Register()
         {
             // TODO: Attribute-based auto registration would be better I guess
-//             TemplateManager.Register(TemplateSampleGenerator.Default);
-//             TemplateManager.Register(NewGameTemplateGenerator.Default);
-//             TemplateManager.Register(ProjectLibraryTemplateGenerator.Default);
-//             TemplateManager.Register(UpdatePlatformsTemplateGenerator.Default);
-//             TemplateManager.Register(AssetFactoryTemplateGenerator.Default);
-//             TemplateManager.Register(AssetFromFileTemplateGenerator.Default);
-//             // Specific asset templates must be registered after AssetFactoryTemplateGenerator
-//             TemplateManager.Register(HeightmapFactoryTemplateGenerator.Default);
-//             TemplateManager.Register(ColliderShapeHullFactoryTemplateGenerator.Default);
-//             TemplateManager.Register(ColliderShapeStaticMeshFactoryTemplateGenerator.Default);
-//             TemplateManager.Register(ProceduralModelFactoryTemplateGenerator.Default);
-//             TemplateManager.Register(SkyboxFactoryTemplateGenerator.Default);
-//             TemplateManager.Register(GraphicsCompositorTemplateGenerator.Default);
-//             TemplateManager.Register(ScriptTemplateGenerator.Default);
-//             TemplateManager.Register(SpriteSheetFromFileTemplateGenerator.Default);
-//             TemplateManager.Register(ModelFromFileTemplateGenerator.Default);
-//             TemplateManager.Register(SkeletonFromFileTemplateGenerator.Default);
-//             TemplateManager.Register(AnimationFromFileTemplateGenerator.Default);
-//             TemplateManager.Register(VideoFromFileTemplateGenerator.Default);
-//             TemplateManager.Register(SoundFromFileTemplateGenerator.Default);
+            TemplateManager.Register(TemplateSampleGenerator.Default);
+            TemplateManager.Register(NewGameTemplateGenerator.Default);
+            TemplateManager.Register(ProjectLibraryTemplateGenerator.Default);
+            TemplateManager.Register(UpdatePlatformsTemplateGenerator.Default);
+            TemplateManager.Register(AssetFactoryTemplateGenerator.Default);
+            TemplateManager.Register(AssetFromFileTemplateGenerator.Default);
+            // Specific asset templates must be registered after AssetFactoryTemplateGenerator
+            TemplateManager.Register(HeightmapFactoryTemplateGenerator.Default);
+            TemplateManager.Register(ColliderShapeHullFactoryTemplateGenerator.Default);
+            TemplateManager.Register(ColliderShapeStaticMeshFactoryTemplateGenerator.Default);
+            TemplateManager.Register(ProceduralModelFactoryTemplateGenerator.Default);
+            TemplateManager.Register(SkyboxFactoryTemplateGenerator.Default);
+            TemplateManager.Register(GraphicsCompositorTemplateGenerator.Default);
+            TemplateManager.Register(ScriptTemplateGenerator.Default);
+            TemplateManager.Register(SpriteSheetFromFileTemplateGenerator.Default);
+            TemplateManager.Register(ModelFromFileTemplateGenerator.Default);
+            TemplateManager.Register(SkeletonFromFileTemplateGenerator.Default);
+            TemplateManager.Register(AnimationFromFileTemplateGenerator.Default);
+            TemplateManager.Register(VideoFromFileTemplateGenerator.Default);
+            TemplateManager.Register(SoundFromFileTemplateGenerator.Default);
         }
     }
 }
