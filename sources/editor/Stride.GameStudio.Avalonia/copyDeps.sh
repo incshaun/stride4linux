47df05be72a4a71f4d4d5939c6521fdb53f5e639
incshaun <mail@nowhere.there>
2024-05-29 08:11:14 +1000

    Temporary fix to copy external libraries into game studio binary directory.

---
+++ sources/editor/Stride.GameStudio.Avalonia/copyDeps.sh
@@ -0,0 +1,14 @@
+cp ../../../externals/BulletSharpPInvoke/src/x64/libbulletc.so bin/Debug/net8.0
+cp ../../../externals/BulletSharpPInvoke/src/x64/libbulletc.so bin/Release/net8.0
+
+cp ../../../externals/BulletSharpPInvoke/src/x64/libVHACD.so bin/Debug/net8.0
+cp ../../../externals/BulletSharpPInvoke/src/x64/libVHACD.so bin/Release/net8.0
+
+cp ../../../externals/FreeImage/libfreeimage-3.16.0.so bin/Debug/net8.0/libFreeImage.so
+cp ../../../externals/FreeImage/libfreeimage-3.16.0.so bin/Release/net8.0/libFreeImage.so
+
+cp ../../../deps/PVRTT/linux-x64/libPVRTexLib.so bin/Debug/net8.0/
+cp ../../../deps/PVRTT/linux-x64/libPVRTexLib.so bin/Release/net8.0/
+
+cp ../../../externals/DirectXTex/linux-x64/libDxtWrapper.so bin/Debug/net8.0/DxtWrapper.so
+cp ../../../externals/DirectXTex/linux-x64/libDxtWrapper.so bin/Release/net8.0/DxtWrapper.so
